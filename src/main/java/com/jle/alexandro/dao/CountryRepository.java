608fb5fa189e45fc5a6f25437ce3bb9b40aa6d84
yanomami <yanomami92@gmail.com>
2019-07-02 12:22:18 +0200

    misc

--- src/main/java/com/jle/alexandro/dao/CountryRepository.java
+++ src/main/java/com/jle/alexandro/dao/CountryRepository.java
@@ -6,6 +6,6 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
-@RepositoryRestResource(collectionResourceRel = "result", path = "countries")
+@RepositoryRestResource(collectionResourceRel = "results", path = "countries")
 public interface CountryRepository extends JpaRepository<CountryEntity, Integer> {
 }
