774f967e81ce38f9afd7afc0af946d909863889b
yanomami <yanomami92@gmail.com>
2019-07-02 16:30:09 +0200

    misc

--- src/main/java/com/jle/alexandro/dao/CountryRepository.java
+++ src/main/java/com/jle/alexandro/dao/CountryRepository.java
@@ -2,18 +2,18 @@ package com.jle.alexandro.dao;
 
 import com.jle.alexandro.models.entities.CountryEntity;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "results", path = "countries")
 public interface CountryRepository extends JpaRepository<CountryEntity, Integer> {
-    @RestResource(path = "/ByDescriptionPage")
-    public List<CountryEntity> findByDescriptionContains(@Param("mc") String description);
+    @RestResource(path = "/byDescription")
+    public Page<CountryEntity> findByDescriptionContains(@Param("desc") String desc, Pageable pageable);
 }
