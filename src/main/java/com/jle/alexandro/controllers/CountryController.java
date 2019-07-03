171c59e513d2a153aef8b683fffeba07adcc8314
yanomami <yanomami92@gmail.com>
2019-07-03 11:29:16 +0200

    misc

--- src/main/java/com/jle/alexandro/controllers/CountryController.java
+++ src/main/java/com/jle/alexandro/controllers/CountryController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// @RestController Versus @RepositoryRestResource
+// example of a @RestController (if business process needed via service) Versus @RepositoryRestResource (which use hateoas)
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
