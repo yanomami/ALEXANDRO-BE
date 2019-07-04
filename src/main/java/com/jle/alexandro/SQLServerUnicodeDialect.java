c9bb0a1018e14227b1e49a56971145f58dbe5453
yanomami <yanomami92@gmail.com>
2019-07-04 21:03:12 +0200

    misc

---
+++ src/main/java/com/jle/alexandro/SQLServerUnicodeDialect.java
@@ -0,0 +1,27 @@
+package com.jle.alexandro;
+
+import org.hibernate.type.StandardBasicTypes;
+
+import java.sql.Types;
+
+public class SQLServerUnicodeDialect extends org.hibernate.dialect.SQLServer2012Dialect {
+    public SQLServerUnicodeDialect() {
+        super();
+        registerColumnType(Types.CHAR, "nchar(1)");
+        registerColumnType(Types.LONGVARCHAR, "nvarchar(max)" );
+        registerColumnType(Types.VARCHAR, 4000, "nvarchar($l)");
+        registerColumnType(Types.VARCHAR, "nvarchar(max)");
+        registerColumnType(Types.CLOB, "nvarchar(max)" );
+
+        registerColumnType(Types.NCHAR, "nchar(1)");
+        registerColumnType(Types.LONGNVARCHAR, "nvarchar(max)");
+        registerColumnType(Types.NVARCHAR, 4000, "nvarchar($l)");
+        registerColumnType(Types.NVARCHAR, "nvarchar(max)");
+        registerColumnType(Types.NCLOB, "nvarchar(max)");
+
+        registerHibernateType(Types.NCHAR, StandardBasicTypes.CHARACTER.getName());
+        registerHibernateType(Types.LONGNVARCHAR, StandardBasicTypes.TEXT.getName());
+        registerHibernateType(Types.NVARCHAR, StandardBasicTypes.STRING.getName());
+        registerHibernateType(Types.NCLOB, StandardBasicTypes.CLOB.getName() );
+    }
+}
