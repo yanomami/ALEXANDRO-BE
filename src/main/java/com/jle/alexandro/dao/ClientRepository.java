6becd56f9d40ba7b1b55e939f4805b751ad77e5e
yanomami <yanomami92@gmail.com>
2019-08-05 20:30:29 +0200

    misc

--- src/main/java/com/jle/alexandro/dao/ClientRepository.java
+++ src/main/java/com/jle/alexandro/dao/ClientRepository.java
@@ -2,10 +2,14 @@ package com.jle.alexandro.dao;
 
 import com.jle.alexandro.models.entities.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RepositoryRestResource(collectionResourceRel = "results")
 public interface ClientRepository extends JpaRepository<Client, Integer> {
+    @RestResource
+    Client findClientByEmailAndPassword(@Param("email") String email, @Param("password") String password);
 }
